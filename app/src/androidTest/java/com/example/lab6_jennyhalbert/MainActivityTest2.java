ae110ceddd808c0b1f407dceed66a92654f5d167
JennyHalbert <jennykatehalbert@gmail.com>
2026-02-24 09:38:41 -0800

    Initial commit

---
+++ app/src/androidTest/java/com/example/lab6_jennyhalbert/MainActivityTest2.java
@@ -0,0 +1,37 @@
+package com.example.lab6_jennyhalbert;
+
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static org.hamcrest.Matchers.allOf;
+
+import android.view.View;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest2 {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void mainActivityTest2() {
+        ViewInteraction frameLayout = onView(
+                allOf(withParent(IsInstanceOf.<View>instanceOf(android.widget.FrameLayout.class)),
+                        isDisplayed()));
+        frameLayout.check(doesNotExist());
+    }
+}
